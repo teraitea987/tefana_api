3c345fddc45a4271dd257f200ed4c66a00499660
Teraitea <proteraitea@gmail.com>
2024-03-22 13:09:26 +0100

    Modification de la table licences

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LicenceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/licence/{id}' , 'App\Http\Controllers\LicenceController@show_pdf');
